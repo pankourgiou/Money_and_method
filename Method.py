06b3aa1168d0d682a0d8b98fbf9d74158ef77eb1
L <60981745+pankourgiou@users.noreply.github.com>
2025-04-11 11:45:15 +0300

    Create Method.py

---
+++ Method.py
@@ -0,0 +1,4 @@
+#Setting E = Money(E usually stands for Energy..) and Method = 1 we have:
+print("E = Money = Method*mcmod2")
+#So next if E = Philosophy...and Method = 1 we have:
+print("Philosophy = Method*mcmod2")
