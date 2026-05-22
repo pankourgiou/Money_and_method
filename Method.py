65fab2054c3e62345c8f9828dac4c21bf691871d
Wagaguya <60981745+pankourgiou@users.noreply.github.com>
2026-05-22 12:04:31 +0300

    Update and rename Method.py to Method.swift

--- Method.py
+++
@@ -1,4 +0,0 @@
-#Setting E = Money(E usually stands for Energy..) and Method = 1 we have:
-print("E = Money = Method*mcmod2")
-#So next if E = Philosophy...and Method = 1 we have:
-print("Philosophy = Method*mcmod2")
